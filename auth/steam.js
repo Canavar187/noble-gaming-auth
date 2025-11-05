f89b44f162c35fcd0189f8f014b2582263f15acd
Canavar187 <c.aydinbas@gmx.de>
2025-11-05 15:17:19 +0100

    Fix: Steam strategy returnURL added

--- auth/steam.js
+++ auth/steam.js
@@ -1,16 +1,20 @@
+// auth/steam.js
 const passport = require('passport');
 const SteamStrategy = require('passport-steam').Strategy;
+require('dotenv').config();
 
-passport.use(new SteamStrategy({
-  returnURL: process.env.STEAM_CALLBACK,
-  realm: process.env.STEAM_REALM,
-  apiKey: process.env.STEAM_API_KEY
-}, function(identifier, profile, done) {
-  const user = {
-    id: profile.id,
-    username: profile.displayName,
-    avatar: profile.photos[2].value,
-    provider: 'Steam'
-  };
-  return done(null, user);
-}));
+passport.use(new SteamStrategy(
+  {
+    returnURL: process.env.STEAM_RETURN_URL,
+    realm: 'https://noble-gaming-auth.onrender.com/',
+    apiKey: process.env.STEAM_API_KEY,
+  },
+  function (identifier, profile, done) {
+    process.nextTick(function () {
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+));
+
+module.exports = passport;
